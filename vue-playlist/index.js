cd844cbd91e94dba7844fe5d05c449ea32843fad
王建 <wangjian9365@autohome.com.cn>
2018-11-28 20:17:03 +0800

    &&

--- vue-playlist/index.js
+++ vue-playlist/index.js
@@ -1,6 +1,17 @@
 var app1 = new Vue({
 	el:'#app1',
 	data:{
-		name:'vue'
+		name:'vue',
+		desc:'getName is vue'
+	},
+	methods:{
+		getName :function(){
+		return this.desc;
+		}
 	}
 })
+/*
+	el(元素) 对应容器id 
+	data: 存储数据
+	methods: 存储方法
+*/
